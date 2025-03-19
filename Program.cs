e9a2749e812268557178bba095f791e1260a807c
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 16:24:42 +0700

    menambahkan class SimpleDatabase

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace modul5_103022300016;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int A = 10;
+        int B = 30;
+        int C = 22;
+        SimpleDataBase<int> db = new SimpleDataBase<int>();
+        db.AddData(A);
+        db.AddData(B);
+        db.AddData(C);
+        db.PrintAllData();
+    }
+}
