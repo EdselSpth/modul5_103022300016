e960449834d783db87401a962aab16643563daef
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-19 15:59:49 +0700

    implementasi-generic-method

---
+++ PemrosesData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul5_103022300016
+{
+    class PemrosesData
+    {
+        public void DapatkanNilaiTerbesar<T>(T a, T b, T c)
+        {
+            dynamic A = (dynamic)a;
+            dynamic B = (dynamic)b;
+            dynamic C = (dynamic)c;
+
+            if (A > B && A > C)
+            {
+                Console.WriteLine("Nilai terbesar adalah " + a);
+            }
+            else if (B > A && B > C)
+            {
+                Console.WriteLine("Nilai terbesar adalah " + b);
+            }
+            else
+            {
+                Console.WriteLine("Nilai terbesar adalah " + c);
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul5_103022300016;
+
+public class Program
+{
+    public static void Main()
+    {
+        int a = 10;
+        int b = 30;
+        int c = 22;
+        PemrosesData pd = new PemrosesData();
+        pd.DapatkanNilaiTerbesar(a, b, c);
+    }
+}
